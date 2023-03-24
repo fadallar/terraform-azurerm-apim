474a70796d396a900d92bc3a34e89863c9be7ad8
Fabrice Dall'ara <fabrice.dallara@hotmail.com>
2023-03-24 12:46:47 +0100

    Adding variable file for timeout variable

--- variables-resource-timeout.tf
+++ variables-resource-timeout.tf
@@ -1,18 +1,18 @@
 variable "create-timeout" {
-    description ="Resource creation timeout"
-    type = string
-    default = "90m"
+  description = "Resource creation timeout"
+  type        = string
+  default     = "90m"
 
 }
 variable "delete-timeout" {
-    description ="Resource deletion timeout"
-    type = string
-    default = "90m"
+  description = "Resource deletion timeout"
+  type        = string
+  default     = "90m"
 
 }
 
 variable "update-timeout" {
-    description ="Resource update timeout"
-    type = string
-    default = "90m"
+  description = "Resource update timeout"
+  type        = string
+  default     = "90m"
 }
